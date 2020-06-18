2c10b7241682c7e73f9b5c463306a048369ec034
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-06-18 14:46:26 +0100

    ready for deployment

---
+++ frontend/build/precache-manifest.9fe66917308cf5adc85952a02006b3fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe38ae13da021ccbd238429e3ff07a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866c6c48a4bae1f05431",
+    "url": "/static/css/main.2c85e910.chunk.css"
+  },
+  {
+    "revision": "4745400509e1d49c16a0",
+    "url": "/static/js/2.401727c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.401727c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866c6c48a4bae1f05431",
+    "url": "/static/js/main.8fa94f6c.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "29ebd1d7c18f0aa128b34d52fc3d729b",
+    "url": "/static/media/BlindDates.29ebd1d7.jpeg"
+  },
+  {
+    "revision": "658c859d510199e51cb840c8c1597786",
+    "url": "/static/media/BlindDatesMain.658c859d.jpeg"
+  }
+]);
